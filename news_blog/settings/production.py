f0b933d7210e8ac376e35f8d5b7df4d3b42d7941
Ajtemir <ajtemirulanbekuulu@gmail.com>
2021-07-16 18:17:51 +0600

    ending end

--- news_blog/settings/production.py
+++ news_blog/settings/production.py
@@ -42,7 +42,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['attractorhheroku.herokuapp.com']
+ALLOWED_HOSTS = ['attractorhheroku.herokuapp.com', 'localhost:8000']
 
 
 # Application definition
@@ -63,7 +63,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleWare',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -152,6 +152,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
